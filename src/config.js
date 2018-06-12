9139fe6c19a5b7cfc5765bfbd56e700a6e4287f2
Manish Kaushik <manish.kaush@gmail.com>
2018-06-12 21:41:25 +0530

    First commit

---
+++ src/config.js
@@ -0,0 +1,9 @@
+var config = () => {
+  return {
+    apiUrl: 'http://localhost:8082/uvschool',
+    clientId: 'webschool-client',
+    clientSecret: '3JSk1KYPM9nAx-ji1'
+  }
+};
+
+export default new config;
